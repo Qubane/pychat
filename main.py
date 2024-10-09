d2c53789ad15c4a5eed6068ea1d09b045da582ae
UltraQbik <no1skill@yandex.ru>
2024-10-09 10:18:44 +0300

    Basic code for application

---
+++ main.py
@@ -0,0 +1,21 @@
+class Application:
+    """
+    Main application for pychat
+    """
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        """
+        Runs the application
+        """
+
+
+def main():
+    app = Application()
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
