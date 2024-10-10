63970ed6095cedfec12d1b35152b2e4adeaa7ea1
UltraQbik <no1skill@yandex.ru>
2024-10-11 01:48:48 +0300

    Add server

--- src/connection.py
+++ src/connection.py
@@ -3,6 +3,7 @@ Connection module for PyChat
 """
 
 
+import ssl
 import asyncio
 
 
@@ -22,3 +23,29 @@ class ClientConnection:
 
         self.reader, self.writer = await asyncio.open_connection(
             host, port)
+
+
+class ServerConnection:
+    """
+    Controls the server hosting the chat
+    """
+
+    def __init__(self):
+        self.server: asyncio.Server | None = None
+
+    async def start_server(self, host: str, port: int, ctx: ssl.SSLContext | None = None):
+        """
+        Starts the server
+        """
+
+        self.server = await asyncio.start_server(
+            host=host, port=port, ssl=ctx,
+            client_connected_cb=self.client_handle)
+
+        async with self.server:
+            await self.server.serve_forever()
+
+    async def client_handle(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        """
+        Server client connection handler
+        """
