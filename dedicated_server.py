1b0e198bb41a275e75dfd91abbdb8be877515eeb
UltraQbik <no1skill@yandex.ru>
2024-10-13 17:57:21 +0300

    Signaling

--- dedicated_server.py
+++ dedicated_server.py
@@ -3,6 +3,7 @@ PyChat server starting file
 """
 
 
+import signal
 import asyncio
 from src.globals import CONNECTION_PORT
 from src.connection import ServerConnection
@@ -16,12 +17,23 @@ class ServerApp(ServerConnection):
     def __init__(self):
         super().__init__()
 
+        # signaling
+        signal.signal(signal.SIGINT, self.stop)
+
     def run(self):
         """
         Runs the application
         """
 
         asyncio.run(self.start_server("", CONNECTION_PORT))
+
+    def stop(self, *args):
+        """
+        Stops the application
+        :param args: fully ignored.
+        """
+
+        self.server.close()
 
 
 def server():
