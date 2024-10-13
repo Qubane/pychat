da792ddd3ab447ab58db252917ed2686b31c2720
UltraQbik <no1skill@yandex.ru>
2024-10-13 18:06:08 +0300

    Update buffer

--- src/globals.py
+++ src/globals.py
@@ -6,5 +6,5 @@ PyChat global constant variables
 CONNECTION_PORT: int = 13700        # port
 CONNECTION_HEARTBEAT: int = 5       # seconds
 
-MESSAGE_RECV_BUFFER: int = 2**20
+MESSAGE_RECV_BUFFER: int = 2**16
 MESSAGE_TERMINATION: bytes = b'\n'
