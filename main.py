5203c0c693dd1f440604dfe956938cb51c1d1826
UltraQbik <no1skill@yandex.ru>
2024-11-03 01:12:44 +0300

    Idk

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ PyChat main starting file
 
 import asyncio
 from src.globals import CONNECTION_PORT, MESSAGE_TERMINATION
-from src.connection import ClientConnection, connect_to_host, send_message
+from src.connection import ClientConnection, connect_to_host, send_message, receive_message
 
 
 class Application:
@@ -15,6 +15,7 @@ class Application:
 
     def __init__(self):
         self.connection: ClientConnection | None = None
+        self.messages: list[str] = list()
 
     def run(self):
         """
@@ -29,10 +30,15 @@ class Application:
         Run coroutine
         """
 
-        # NOTE: temporary code, must be removed later
-        self.connection = await connect_to_host("127.0.0.1", CONNECTION_PORT)  # connect to loopback
+        address = input("Enter address: ")
+        try:
+            self.connection = await connect_to_host(address, CONNECTION_PORT)
+        except asyncio.TimeoutError:
+            return
 
-        await send_message(self.connection, b'Hello world')
+        await send_message(self.connection, b'Random user joined!')
+        while True:
+            print(await receive_message(self.connection))
 
 
 def main():
