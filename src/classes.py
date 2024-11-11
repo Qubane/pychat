c4ad7f6ff0c419970787862c24c9c28d129a128a
UltraQbik <no1skill@yandex.ru>
2024-11-11 22:38:27 +0300

    Simplify classes to use @dataclass

--- src/classes.py
+++ src/classes.py
@@ -3,42 +3,26 @@ PyChat class definitions
 """
 
 
+from dataclasses import dataclass
+
+
+@dataclass
 class User:
     """
     Basic user class
     """
 
-    def __init__(self, user_id: int, nickname: str, **kwargs):
-        self.user_id: int = user_id
-        self.nickname: str = nickname
-        self.description: str = "no description"
-
-        for arg, val in kwargs.items():
-            setattr(self, arg, val)
-
-    def update_info(self, **kwargs):
-        """
-        Updates information about the user
-        :param kwargs: keyword arguments
-        """
-
-        for arg, val in kwargs.items():
-            if arg == "user_id":
-                raise Exception("Unable to change user id for existing user")
-            setattr(self, arg, val)
+    user_id: int
+    nickname: str
+    description: str
 
 
+@dataclass(frozen=True)
 class Message:
     """
     Basic message class
     """
 
-    def __init__(self, user_id: int, message: str, **kwargs):
-        self.user_id: int = user_id
-        self.message: str = message
-
-        for arg, val in kwargs.items():
-            setattr(self, arg, val)
-
-    def __repr__(self):
-        return f"{{msg by {self.user_id}}}"
+    user_id: int
+    message_id: int
+    content: str
