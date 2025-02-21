1d16e8444ec873c8571ac6f39fac7e1b832405be
UltraQbik <no1skill@yandex.ru>
2025-02-21 11:33:05 +0300

    Add basic classes

--- source/classes.py
+++ source/classes.py
@@ -1,3 +1,26 @@
 """
 PyChat classes
 """
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    """
+    Class containing user data
+    """
+
+    username: str
+    description: str
+
+
+@dataclass(frozen=True)
+class Message:
+    """
+    Class containing message data
+    """
+
+    content: str
+    author: str
